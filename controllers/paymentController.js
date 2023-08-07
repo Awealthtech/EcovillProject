a534b4c593c0ac1246371c18ef2b4674f03d04d2
Okunade Adeola <adeolafunke7@gmail.com>
2023-08-07 10:56:22 +0100

    updated

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,29 +1,20 @@
 // controllers/paymentController.js
-const axios = require("axios");
-const Payment = require("../models/payment");
-const dotenv = require("dotenv");
+const axios = require("axios")
+const Payment = require('../models/payment');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const paystackApi = axios.create({
-  baseURL: "https://api.paystack.co",
+  baseURL: 'https://api.paystack.co',
   headers: {
     Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
   },
 });
 
-const makePaymentGet = (req, res) => {
-  return res.render("pay", { error: "" });
-};
-
-async function makePayment(req, res) {
+async function initiatePayment(req, res) {
   try {
     const { email, amount } = req.body;
-    const existingUser = await userSchema.findOne({ email });
-    if (!existingUser) {
-      res.render("pay", { error: "user details incorrect" });
-    }
-
-    const response = await paystackApi.post("/transaction/initialize", {
+    const response = await paystackApi.post('/transaction/initialize', {
       email,
       amount: amount * 100, // Paystack expects amount in kobo (i.e., 100 kobo = 1 Naira)
     });
@@ -33,45 +24,128 @@ async function makePayment(req, res) {
       email,
       amount,
       reference: response.data.data.reference,
-      status: "pending",
+      status: 'pending',
     });
     await payment.save();
-    res.redirect({ authorization_url: response.data.data.authorization_url });
+
+    res.redirect(response.data.data.authorization_url);
   } catch (error) {
     console.error(error);
-    res.render("pay", {
-      error: "An error occurred while initializing payment.",
-    });
+    res.render('error', { message: 'An error occurred while initializing payment.' });
   }
 }
 
 async function verifyPayment(req, res) {
   try {
     const { reference } = req.query;
-    const verifyResponse = await paystackApi.get(
-      `/transaction/verify/${reference}`
-    );
-    if (verifyResponse.data.data.status === "success") {
+    const verifyResponse = await paystackApi.get(`/transaction/verify/${reference}`);
+    if (verifyResponse.data.data.status === 'success') {
       const payment = await Payment.findOneAndUpdate(
         { reference },
-        { status: "success" },
+        { status: 'success' },
         { new: true }
       );
       if (!payment) {
-        return res.render("error");
+        return res.render('error', { message: 'Payment not found.' });
       }
-      res.render("success");
+      res.render('success', { message: 'Payment verified successfully.' });
     } else {
-      res.render("error");
+      res.render('error', { message: 'Payment verification failed.' });
     }
   } catch (error) {
     console.error(error);
-    res.render("error");
+    res.render('error', { message: 'An error occurred while verifying payment.' });
   }
 }
 
 module.exports = {
-  makePayment,
+  initiatePayment,
   verifyPayment,
-  makePaymentGet,
 };
+
+
+
+
+
+
+
+
+
+
+// // controllers/paymentController.js
+// const axios = require("axios");
+// const Payment = require("../models/payment");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const paystackApi = axios.create({
+//   baseURL: "https://api.paystack.co",
+//   headers: {
+//     Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+//   },
+// });
+
+// const makePaymentGet = (req, res) => {
+//   return res.render("pay", { error: "" });
+// };
+
+// async function makePayment(req, res) {
+//   try {
+//     const { email, amount } = req.body;
+//     const existingUser = await userSchema.findOne({ email });
+//     if (!existingUser) {
+//       res.render("pay", { error: "user details incorrect" });
+//     }
+
+//     const response = await paystackApi.post("/transaction/initialize", {
+//       email,
+//       amount: amount * 100, // Paystack expects amount in kobo (i.e., 100 kobo = 1 Naira)
+//     });
+
+//     // Save payment details to MongoDB
+//     const payment = new Payment({
+//       email,
+//       amount,
+//       reference: response.data.data.reference,
+//       status: "pending",
+//     });
+//     await payment.save();
+//     res.redirect({ authorization_url: response.data.data.authorization_url });
+//   } catch (error) {
+//     console.error(error);
+//     res.render("pay", {
+//       error: "An error occurred while initializing payment.",
+//     });
+//   }
+// }
+
+// async function verifyPayment(req, res) {
+//   try {
+//     const { reference } = req.query;
+//     const verifyResponse = await paystackApi.get(
+//       `/transaction/verify/${reference}`
+//     );
+//     if (verifyResponse.data.data.status === "success") {
+//       const payment = await Payment.findOneAndUpdate(
+//         { reference },
+//         { status: "success" },
+//         { new: true }
+//       );
+//       if (!payment) {
+//         return res.render("error");
+//       }
+//       res.render("success");
+//     } else {
+//       res.render("error");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.render("error");
+//   }
+// }
+
+// module.exports = {
+//   makePayment,
+//   verifyPayment,
+//   makePaymentGet,
+// };
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,11 +3,34 @@ const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 
-// Make payment
-router.get('/pay', paymentController.makePaymentGet);
-router.post('/pay', paymentController.makePayment);
+// Render payment form
+router.get('/pay', (req, res) => {
+  res.render('pay');
+});
+
+// Initiate payment
+router.post('/initiate-payment', paymentController.initiatePayment);
 
 // Verify payment
 router.get('/verify-payment', paymentController.verifyPayment);
 
 module.exports = router;
+
+
+
+
+
+// // routes/paymentRoutes.js
+// const express = require('express');
+// const router = express.Router();
+// const paymentController = require('../controllers/paymentController');
+
+// // Make payment
+// router.get('/pay', paymentController.makePaymentGe
+// );
+// router.post('/pay', paymentController.makePayment);
+
+// // Verify payment
+// router.get('/verify-payment', paymentController.verifyPayment);
+
+// module.exports = router;
