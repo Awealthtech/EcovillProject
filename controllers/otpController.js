5543fa1ede591672c61b96dc3d02e1f32e9e32b4
Okunade Adeola <adeolafunke7@gmail.com>
2023-08-07 09:25:21 +0100

    updated

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -52,74 +52,26 @@ class OTPController {
   }
 
   static async verifyOTP(req, res) {
-    const { email, otp } = req.body;
+    try {
+      const { email, otp } = req.body;
 
-    // Find the OTP from the database
-    const savedOTP = await OTP.findOne({ email }).sort({ createdAt: -1 });
+      const savedOTP = await OTP.findOne({ email }).sort({ createdAt: -1 });
 
-    if (!savedOTP) {
-      res.render("otpVerification", { error: "invalid" });
-    }
+      if (!savedOTP) {
+        res.render("otpVerification", { error: "invalid" });
+      }
 
-    if (otp === savedOTP.otp) {
-      // OTP is valid
-      res.redirect( "/api/signup",200, { error: "" }) 
-      console.log("otp verified")
-    } else {
-      // Invalid OTP
+      if (otp === savedOTP.otp){
+        // OTP is valid
+        res.redirect( "/api/signup",200, { error: "" }) 
+        console.log("otp verified")
+      } else {
+        // Invalid OTP
+        res.render("otpVerification", { error: "invalid" });
+      }
+    } catch (error) {
       res.render("otpVerification", { error: "invalid" });
     }
   }
-
-  // resend otp
-//   static async resendOTP(req, res) {
-//     const { email } = req.body;
-
-//     // Find the OTP from the database
-//     const savedOTP = await OTP.findOne({ email }).sort({ createdAt: -1 });
-
-//     if (!savedOTP) {
-//       res.render("otpVerification", { error: "invalid" });
-//     }
-
-//     if (email === savedOTP.email) {
-//       const otp = Math.floor(100000 + Math.random() * 900000); // Generate a random 6-digit OTP
-
-//       // Save the OTP to the database
-//       const otpInstance = new OTP({ email, otp });
-//       await otpInstance.save();
-  
-//       // Send the OTP via email using Nodemailer
-//       const transporter = nodemailer.createTransport({
-//         service: "gmail",
-//         auth: {
-//           user: process.env.AUTH_EMAIL,
-//           pass: process.env.AUTH_PASSWORD,
-//         },
-//       });
-  
-//       const mailOptions = {
-//         from: process.env.AUTH_EMAIL,
-//         to: email,
-//         subject: "Your OTP",
-//         text: `Your OTP is ${otp}`,
-//       };
-  
-//       try {
-//         await transporter.sendMail(mailOptions);
-//         console.log("OTP sent via email");
-//         res.redirect(200, "/verifyOTP", { error: "" }) 
-//       } catch (error) {
-//         console.log("Error sending OTP:", error);
-//         res.render("index", { error: "existing user" });
-//       }
-//       // OTP is valid
-//       // res.redirect(200, "/api/signup", { error: "" }) 
-//       // console.log("otp verified")
-//     } else {
-//       // Invalid OTP
-//       res.render("otpVerification", { error: "invalid" });
-//     }
-// }
 }
 module.exports = OTPController;
